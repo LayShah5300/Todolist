8db66fef550f4f8b88d1116d1cda5b3d34911498
LayShah5300 <layshahbhuj@gmail.com>
2021-03-04 12:41:01 +0530

    delete and check unit tested

--- js/deleteCheck.test.js
+++ js/deleteCheck.test.js
@@ -1,11 +1,6 @@
 const deleteCheck = require("./deletetCheck");
 
 
-// test('delete from list', () => {
-//     e.target === "trash-btn";
-    
-//     expect(deleteCheck()).toBe(3);
-// });
 
 it("displays another css when task complete button clicked", () => {
     const $ = require("jquery");
